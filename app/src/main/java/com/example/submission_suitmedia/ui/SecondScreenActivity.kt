4b987c0611c5b5257bbe8888aad274c045e7b95b
Hilda003 <Hildatulwardah003@gmail.com>
2024-08-10 11:59:19 +0700

    fix status bar cannot display

--- app/src/main/java/com/example/submission_suitmedia/ui/SecondScreenActivity.kt
+++ app/src/main/java/com/example/submission_suitmedia/ui/SecondScreenActivity.kt
@@ -16,14 +16,14 @@ class SecondScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivitySecondScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar?.hide()
         binding.buttonBack.setOnClickListener {
             finish()
         }
